589ac510cb844f180b964e064fd4b184add3dec9
Luiz Zanini <l.zanini@semrush.com>
2018-09-11 16:43:28 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='Plotly wrapper',
+    url='https://github.com/luizzan/plotly_wrapper',
+    author='Luiz Zanini',
+    author_email='plotly_wrapper@luizzanini.com',
+    packages=['__init__'],
+)
